aeb9f9268e8d89f13a429a5a3f5635de270ee634
John Davalos <john.davalos@gmail.com>
2013-09-26 07:04:07 -0700

    added validations for coordinates

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -1,3 +1,14 @@
 class Coordinate < ActiveRecord::Base
+  #=============
+  # associations
+  # =============
   belongs_to :earthquake
+
+  #=============
+  # validations
+  #=============
+  validates :logitude, numericality: { only_integer: false }
+  validates :latitude, numericality: { only_integer: false }
+  validates :depth, numericality: { only_integer: false }
+
 end
